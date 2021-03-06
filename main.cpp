f742fb249417c28181c594e42bc7f7d9f0c36a91
Санданов Тимур Александрович <ip012s26@bwp02.csc.local>
2021-03-06 12:41:40 +0700

    clang-format; makefile; .gitignore

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << " a b c d e f g h " << endl;
+  cout << "1               1" << endl;
+  cout << "2               2" << endl;
+  cout << "3               3" << endl;
+  cout << "4               4" << endl;
+  cout << "5               5" << endl;
+  cout << "6               6" << endl;
+  cout << "7               7" << endl;
+  cout << "8               8" << endl;
+  cout << " a b c d e f g h " << endl;
+  return 0;
+}
+ 
---
+++ makefile
@@ -0,0 +1,5 @@
+all: main run
+main: main.cpp
+	g++-Wall-Werror -o main main.cpp
+run:
+	./main
