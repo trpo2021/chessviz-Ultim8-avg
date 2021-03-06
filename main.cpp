6d0de4657b68dd9d9f34c5b440609461e0f63065
Санданов Тимур Александрович <ip012s26@bwp02.csc.local>
2021-03-06 13:04:51 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-  cout << " a b c d e f g h " << endl;
-  cout << "1               1" << endl;
-  cout << "2               2" << endl;
-  cout << "3               3" << endl;
-  cout << "4               4" << endl;
-  cout << "5               5" << endl;
-  cout << "6               6" << endl;
-  cout << "7               7" << endl;
-  cout << "8               8" << endl;
-  cout << " a b c d e f g h " << endl;
+  cout << "  a b c d e f g h  " << endl;
+  cout << "1 Z X C     C X Z 1" << endl;
+  cout << "2 P P P P P P P P 2" << endl;
+  cout << "3                 3" << endl;
+  cout << "4                 4" << endl;
+  cout << "5                 5" << endl;
+  cout << "6                 6" << endl;
+  cout << "7 p p p p p p p p 7" << endl;
+  cout << "8 z x c   c  x  z 8" << endl;
+  cout << "  a b c d e f g h  " << endl;
   return 0;
 }
  
